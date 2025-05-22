9e0ad73e0261648b6a47ee973a1ef9de88ec90e7
Charles Kariuki <charlymburu@gmail.com>
2025-05-22 20:10:04 +0300

    Add functions and error handling to port scanner

--- 1.simple_port_scanner.py
+++ 1.simple_port_scanner.py
@@ -1,39 +1,32 @@
 import socket
 import time
 
-#Defines the target IP (localhost for safety):
-target_ip= "127.0.0.1"
-
-#list of common ports to scan:
-ports_to_scan = [22, 80, 443, 8000]
-
-#print a welcome message:
-print("Starting port scanner for", target_ip)
-
-#Loop through each port
-for port in ports_to_scan:
-
-    #create a socket object
+def scan_port(ip, port):
+  try:      
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    #Set a timeout to avoid hanging
     sock.settimeout(1)
-
-    #Try connecting to the IP and port
-    result = sock.connect_ex((target_ip, port))
-
-    #check if port is open (result == 0 means success)
+    result = sock.connect_ex((ip, port))
     if result == 0:
         print(f"port {port} is OPEN")
     else:
-        print(f"port {port} is CLOSED")
+        print(f"Port {port} is CLOSED")
+    sock.close()
 
-    #Close the socket
-    sock.close() 
+  except Exception as e:
+     print(f"Error scanning port {port}: {e}")
+  finally:
+     sock.close()
 
-    #small delapy to avoid overwhelming the system
+# Define the target IP (localhost for safety) and ports to scan.
+target_ip = "127.0.0.1"
+ports_to_scan = [22, 80, 443, 8000]
+
+print("Starting port scanner for", target_ip)
+
+#loop through each port
+for port in ports_to_scan:
+    scan_port(target_ip, port)
     time.sleep(0.5)
 
 print("Scan complete!")    
-
 
